e310f5c3625cebf666f83d407e6e53484bf70917
NathanM <thenathanmills@gmail.com>
2020-06-29 21:31:38 +1200

    Added initial task interface with scripts for dragging in screens

---
+++ PolymorphismVisualisation/Assets/DragDrop/Scripts/DropRegion.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class DropRegion : MonoBehaviour, IDropHandler {
+
+    public Image _screenImage;
+    public Image _objectImage;
+
+    public void OnDrop(PointerEventData eventData) {
+        if (eventData.pointerDrag != null) {
+            if (eventData.pointerDrag.tag.Equals("Screen"))
+            {
+                _screenImage.sprite = eventData.pointerDrag.GetComponent<Image>().sprite;
+            }
+        }
+    }
+
+}
