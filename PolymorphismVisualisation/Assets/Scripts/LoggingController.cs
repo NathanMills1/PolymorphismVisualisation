c9bf6ee11a094f11e7b96f65ee7e48499a712d9a
unknown <awan968@aucklanduni.ac.nz>
2020-08-27 16:58:22 +1200

    logging

---
+++ PolymorphismVisualisation/Assets/Scripts/LoggingController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class LoggingController : MonoBehaviour
+{
+
+    private readonly string baseURL = "https://oop-visualisation.herokuapp.com/stats";
+    private string token = "";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(GetToken());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator GetToken()
+    {
+        string URL = "https://oop-visualisation.herokuapp.com/token";
+
+        using (UnityWebRequest www = UnityWebRequest.Get(URL))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                if (www.isDone)
+                {
+                    // handle the result
+                    token = System.Text.Encoding.UTF8.GetString(www.downloadHandler.data);
+                    Debug.Log(token);
+                }
+                else
+                {
+                    //handle the problem
+                    Debug.Log("Error! data couldn't get.");
+                }
+            }
+        }
+    }
+
+    public void MakeWebReq()
+    {
+        StartCoroutine(Inc());
+    }
+
+    IEnumerator Inc()
+    {
+        string URL = baseURL + "/correct?token=" + token;
+
+        UnityWebRequest req = UnityWebRequest.Get(URL);
+
+        yield return req.SendWebRequest();
+
+        if (req.isNetworkError || req.isHttpError)
+        {
+            Debug.LogError(req.error);
+            yield break;
+        }
+    }
+}
